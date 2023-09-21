614125d488d4320b50ff6a102e6f5b936e205df8
meg <65887771+9rotama@users.noreply.github.com>
2023-09-21 17:28:14 +0900

    ボタン作成

--- Assets/Scripts/UI/ButtonView.cs
+++ Assets/Scripts/UI/ButtonView.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ButtonView : MonoBehaviour
 {
+    private Animator animator;
+
+    private Button button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
+    public void ButtonActive()
+    {
+        animator.SetBool("scaleUp", true);
+    }
+
+    public void ButtonNoActive()
+    {
+        animator.SetBool("scaleUp", false);
+
+    }
     // Update is called once per frame
     void Update()
     {
